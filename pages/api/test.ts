8337bd627361a8f4df3a98b052dd0f71c4744b35
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-05-16 14:52:11 -0400

    update test fn

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,5 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: 'John Doe' })
+  const { email } = req.body
+  const FORM_ID = process.env.CONVERTKIT_FORM_ID
+  const API_KEY = process.env.CONVERTKIT_API_KEY
+  const API_URL = 'https://api.convertkit.com/v3/'
+  const data = { email, api_key: API_KEY }
+
+  const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  })
+
+  return response
 }
