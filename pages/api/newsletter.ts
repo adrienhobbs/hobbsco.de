b3505dcb92022af4e693cfd2c64afa990a651f42
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-05-16 15:30:32 -0400

    update newsletter api

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,13 +1,21 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default NewsletterAPI({
-  provider: siteMetadata.newsletter.provider,
-})
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { email } = req.body
+  const FORM_ID = process.env.CONVERTKIT_FORM_ID
+  const API_KEY = process.env.CONVERTKIT_API_KEY
+  const API_URL = 'https://api.convertkit.com/v3/'
+  const data = { email, api_key: API_KEY }
 
-// export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-//   // Do whatever you want here, before the request is passed down
-//   return await NewsletterAPI(req, res, {
-//     provider: ...
-//   })
-// }
+  const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  })
+
+  const resp = await response.json()
+
+  res.status(201).json(resp)
+}
